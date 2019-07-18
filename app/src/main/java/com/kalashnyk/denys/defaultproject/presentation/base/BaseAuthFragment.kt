6ef4e1ac8ecee092ed24234b0d68c436b2d9bdce
kalashnykdenys <kalashnyk.denys@gmail.com>
2019-07-18 21:09:14 +0300

    update logic of auth flow

---
+++ app/src/main/java/com/kalashnyk/denys/defaultproject/presentation/base/BaseAuthFragment.kt
@@ -0,0 +1,15 @@
+package com.kalashnyk.denys.defaultproject.presentation.fragments
+
+import android.os.Bundle
+import com.kalashnyk.denys.defaultproject.presentation.activities.auth.flow.AuthFlowModel
+import com.kalashnyk.denys.defaultproject.presentation.base.BaseFragment
+
+abstract class BaseAuthFragment: BaseFragment() {
+
+    protected var authFlowModel : AuthFlowModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        authFlowModel = AuthFlowModel()
+    }
+}

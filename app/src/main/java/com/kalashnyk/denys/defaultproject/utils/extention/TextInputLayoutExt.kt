5f4a3541e2c9a4126ac9847a0aac7b6d3e69607b
Kalashnyk Denys <kalashnyk.denys@gmail.com>
2019-08-23 10:20:05 +0300

    update

--- app/src/main/java/com/kalashnyk/denys/defaultproject/utils/extention/TextInputLayoutExt.kt
+++ app/src/main/java/com/kalashnyk/denys/defaultproject/utils/extention/TextInputLayoutExt.kt
@@ -4,7 +4,7 @@ import com.google.android.material.textfield.TextInputLayout
 
 fun TextInputLayout.clearErrorForTextInputLayout() {
     this.apply {
-        this.isHintEnabled=true
+//        this.isHintEnabled=true
         this.error=null
         this.isErrorEnabled=false
     }
@@ -12,7 +12,7 @@ fun TextInputLayout.clearErrorForTextInputLayout() {
 
 fun TextInputLayout.addErrorForTextInputLayout(error: String?) {
     this.apply {
-        this.isHintEnabled=false
+//        this.isHintEnabled=false
         this.isErrorEnabled=true
         this.error=error
     }
